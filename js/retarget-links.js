519096d7253d0a51e2f7881efb9f6f7e118b6c45
Erik Gillespie <erik.gillespie@gmail.com>
2015-10-11 22:37:16 -0400

    Open external sites in new tab

---
+++ js/retarget-links.js
@@ -0,0 +1,16 @@
+(function() {
+    var siteUrl = 'http://technicalrex.com';
+    var siteUrlLen = siteUrl.length;
+    var links = document.getElementsByTagName('a');
+    var len = links.length;
+    for (var i = 0; i < len; i++) {
+        var link = links[i];
+        console.log(link.href);
+        console.log(link.href.slice(0, 5));
+        console.log(link.href.slice(0, siteUrlLen));
+        if ((link.href.slice(0, 5) === 'http:' || link.href.slice(0, 6) === 'https:')
+                && link.href.slice(0, siteUrlLen) !== siteUrl) {
+            link.target = '_blank';
+        }
+    }
+})();
